1814454429dbbca2f5456eb6be9314692db4170e
Alan Maizon <58921943+alanmaizon@users.noreply.github.com>
2025-06-25 13:24:52 +0100

    Update App.jsx

--- melisa-corso-website/src/App.jsx
+++ melisa-corso-website/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-// Custom global styles (will be imported via main.jsx too)
 import './index.css';
 
-// Import individual components from the components folder
 import NavbarComponent from './components/NavbarComponent';
 import HeroSection from './components/HeroSection';
 import ServicesSection from './components/ServicesSection';
 import AboutSection from './components/AboutSection';
 import ContactSection from './components/ContactSection';
-import TestimonialsSection from './components/TestimonialsSection'; // NEW IMPORT
+import TestimonialsSection from './components/TestimonialsSection';
 
 function App() {
   return (
@@ -16,10 +14,7 @@ function App() {
       <NavbarComponent />
 
       {/* Home Page */}
-      <HeroSection
-        id="home"
-        backgroundImage="/images/hero-desktop.jpg" // Now using the main desktop hero image
-      >
+      <HeroSection id="home"> {/* backgroundImage prop removed */}
         <h1><img src="/images/logo.png" alt="f-hole round violin logo" /></h1>
         <h1>Melisa Corso</h1>
         <h4>Sharing the Beauty of Violin Music</h4>
@@ -29,27 +24,27 @@ function App() {
       <ServicesSection id="services" />
 
       {/* Hero Space 1 */}
-      <HeroSection className="hero-space" backgroundImage="/images/hero-desktop.jpg" /> {/* Using the main desktop hero image */}
+      <HeroSection className="hero-space" /> {/* backgroundImage prop removed */}
 
       {/* About Page */}
       <AboutSection id="about" />
 
-      {/* NEW: Testimonials Section */}
+      {/* Testimonials Section */}
       <TestimonialsSection id="testimonials" />
 
-      {/* Hero Space 2 (adjusting the image here to accommodate the new section) */}
-      <HeroSection className="hero-space" backgroundImage="/images/hero-desktop.jpg" /> {/* Using the main desktop hero image */}
+      {/* Hero Space 2 */}
+      <HeroSection className="hero-space" /> {/* backgroundImage prop removed */}
 
       {/* Contact Page */}
       <ContactSection id="contact" />
 
       {/* Footer */}
-      <HeroSection className="hero-section hero-large" backgroundImage="/images/hero-desktop.jpg"> {/* Using the main desktop hero image */}
+      <HeroSection className="hero-section hero-large"> {/* backgroundImage prop removed */}
         <a href="#home">
           <img src="/images/logo.png" alt="f-hole round violin logo" style={{ height: '80px', marginBottom: '20px' }} />
         </a>
         <div className="footer-content">
-          <small>Melisa Corso 2025 © All rights reserved</small>
+          <small>Melisa Corso 2024 © All rights reserved</small>
         </div>
       </HeroSection>
     </div>
