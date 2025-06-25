feecb365de20a9d42ad62fceeccb800451fad4a8
Alan Maizon <alanmaizon@hotmail.com>
2025-06-25 15:18:32 +0100

    Refactor hero sections and update styles for improved responsiveness and visual consistency

--- melisa-corso-website/src/App.jsx
+++ melisa-corso-website/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './index.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faWhatsapp, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+
 import NavbarComponent from './components/NavbarComponent';
 import HeroSection from './components/HeroSection';
 import ServicesSection from './components/ServicesSection';
@@ -14,7 +18,10 @@ function App() {
       <NavbarComponent />
 
       {/* Home Page */}
-      <HeroSection id="home"> {/* backgroundImage prop removed */}
+      <HeroSection
+        id="home"
+        backgroundImage="/images/hero-desktop.jpg" // RE-ADDED: Pointing to desktop image
+      >
         <h1><img src="/images/logo.png" alt="f-hole round violin logo" /></h1>
         <h1>Melisa Corso</h1>
         <h4>Sharing the Beauty of Violin Music</h4>
@@ -24,7 +31,7 @@ function App() {
       <ServicesSection id="services" />
 
       {/* Hero Space 1 */}
-      <HeroSection className="hero-space" /> {/* backgroundImage prop removed */}
+      <HeroSection className="hero-space" backgroundImage="/images/hero-desktop.jpg" /> {/* RE-ADDED */}
 
       {/* About Page */}
       <AboutSection id="about" />
@@ -33,13 +40,13 @@ function App() {
       <TestimonialsSection id="testimonials" />
 
       {/* Hero Space 2 */}
-      <HeroSection className="hero-space" /> {/* backgroundImage prop removed */}
+      <HeroSection className="hero-space" backgroundImage="/images/hero-desktop.jpg" /> {/* RE-ADDED */}
 
       {/* Contact Page */}
       <ContactSection id="contact" />
 
       {/* Footer */}
-      <HeroSection className="hero-section hero-large"> {/* backgroundImage prop removed */}
+      <HeroSection className="hero-section hero-large" backgroundImage="/images/hero-desktop.jpg"> {/* RE-ADDED */}
         <a href="#home">
           <img src="/images/logo.png" alt="f-hole round violin logo" style={{ height: '80px', marginBottom: '20px' }} />
         </a>
